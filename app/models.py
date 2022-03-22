b9b10a70e87823eda29e8f5d634c023bc421c029
Abdirahman <dubowhamaan@gmail.com>
2022-03-22 16:26:25 +0300

    adding models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True, blank=True)
+    image = CloudinaryField('image')
+    description = models.CharField(max_length=400,default=False)
+    author = models.CharField(default='Author name..',max_length=100)
+    year_published = models.IntegerField(blank=True, null=True)
+   
+   
+    def __str__(self):
+      return self.name
+
+    @property
+    def save_image(self):
+          self.save()
+
+    def delete_image(self):
+        self.delete()
