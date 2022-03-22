d7591d1d00324a2bc8584b2f19322eb6c650e3e3
Abdirahman <dubowhamaan@gmail.com>
2022-03-22 16:24:48 +0300

    adding views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .forms import *
+from .models import *
+from django.http import HttpResponseRedirect 
 # Create your views here.
+def index(request):
+
+
+	products = Product.objects.all()
+	if request.method == 'POST':
+			form = ProductForm(request.POST, request.FILES)
+			if form.is_valid():
+					post = form.save(commit=False)
+					post.user = request.user
+					post.save()
+					return HttpResponseRedirect(request.path_info)
+	else:
+			form = ProductForm()
+	params = {
+        'products':products,
+				'form': form, 
+       
+        }
+	return render(request, 'index.html', params)
